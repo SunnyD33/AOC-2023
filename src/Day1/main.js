9b24bb68a3eb29a06d41ae3f967da663ebaab2fb
Quincy Williams <QLWilliams@student.fullsail.edu>
2023-12-03 02:58:09 -0500

    Initial build and commit

---
+++ src/Day1/main.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+//Check that tsc file has been created and that types/node is installed
+var fs = require("fs");
+// Parse data
+var data = fs.readFileSync("./input.txt", "utf-8");
+var dataByLine = data.split("\n");
+//console.log(dataByLine);
+// Solve puzzle from parsed data
+var isAlpha = /([A-Za-z])+/gi;
+var sum = 0;
+var numArr = [];
+dataByLine.forEach(function (line) {
+    var cleanedString = line.replaceAll(isAlpha, "");
+    numArr.push(cleanedString);
+});
+numArr.forEach(function (num) {
+    if (num.length === 2) {
+        sum += parseInt(num);
+    }
+    else {
+        var firstlast = "".concat(num[0]).concat(num[num.length - 1]);
+        sum += parseInt(firstlast);
+    }
+});
+console.log(sum);
---
+++ src/Day1/main.ts
@@ -0,0 +1,28 @@
+//Check that tsc file has been created and that types/node is installed
+import * as fs from "fs";
+
+// Parse data
+const data = fs.readFileSync("./input.txt", "utf-8");
+const dataByLine = data.split("\n");
+//console.log(dataByLine);
+
+// Solve puzzle from parsed data
+const isAlpha = /([A-Za-z])+/gi;
+let sum: number = 0;
+let numArr: string[] = [];
+
+dataByLine.forEach((line) => {
+  const cleanedString = line.replaceAll(isAlpha, "");
+  numArr.push(cleanedString);
+});
+
+numArr.forEach((num) => {
+  if (num.length === 2) {
+    sum += parseInt(num);
+  } else {
+    let firstlast: string = `${num[0]}${num[num.length - 1]}`;
+    sum += parseInt(firstlast);
+  }
+});
+
+console.log(sum);
